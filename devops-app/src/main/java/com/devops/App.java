d77c035c348f96ecc36268616656aa2eab70bb1a
Adiba Khattabi <hamza@aznaytechnologies.com>
2026-01-12 01:16:36 +0100

    print modif

--- devops-app/src/main/java/com/devops/App.java
+++ devops-app/src/main/java/com/devops/App.java
@@ -2,6 +2,6 @@ package com.devops;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Bonjour Monsieur j'espere que cette application fonctionne correctement !");
+        System.out.println("Bonjour et bon courage dans votre projet en DevOps");
     }
 }
